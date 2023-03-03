5731bbc56f31a2b4e8d247fe8e774341f49dc945
piligr1m <stanislav.m.martynov@gmail.com>
2023-03-03 19:58:30 +0300

    Update

--- LW1/src/Main.java
+++ LW1/src/Main.java
@@ -40,6 +40,7 @@ public class Main {
                 matrix3[i][j] = -matrix[i][j];
             }
         }
+        matrix3 = transpose(matrix3);
         System.out.println("Игрок 2");
         BR_alg brAlg1 = new BR_alg(matrix3, limit);
         if(brAlg1.setInitV2())
@@ -51,10 +52,20 @@ public class Main {
         {
             for (int j = 0; j < 3; j++)
             {
-                matrix4[i][j] = (float)-matrix[i][j];
+                matrix4[i][j] = (float)matrix3[i][j];
             }
         }
-        AnalyticAlg analyticAlg2 = new AnalyticAlg();
         analyticAlg.countXYV(matrix4);
+    }
+    public static int[][] transpose(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        return matrix;
     }
 }
